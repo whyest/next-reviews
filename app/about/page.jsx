f0f984719351da5630ca38c28f13971103f5ee03
Денис Галкин <whyest@gmail.com>
2023-10-09 22:32:53 +0300

    added about page and reviews pages

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+const AboutPage = () => {
+	return (
+		<>
+			<h1>AboutPage</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, aut
+				veniam fuga eos beatae atque laudantium neque maxime, tempora, et ab
+				minima fugit mollitia unde in facere eaque quod eveniet.
+			</p>
+		</>
+	)
+}
+export default AboutPage
---
+++ app/reviews/hollow-knight/page.jsx
@@ -0,0 +1,13 @@
+const HollowKnightPage = () => {
+	return (
+		<>
+			<h1>Hollow Knight page</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+				ipsum autem quaerat accusantium dignissimos nam magni mollitia rem, id
+				consequuntur!
+			</p>
+		</>
+	)
+}
+export default HollowKnightPage
