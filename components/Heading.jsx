79a93524faf170ee3334af1b46c6a693b80f80b3
Денис Галкин <whyest@gmail.com>
2023-10-10 23:18:05 +0300

    add custom font to heading

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,4 +1,9 @@
+import { orbitron } from '@/app/fonts'
 const Heading = ({ children }) => {
-	return <h1 className='font-bold text-2xl pb-3'>{children}</h1>
+	return (
+		<h1 className={`font-bold text-2xl pb-3 ${orbitron.className}`}>
+			{children}
+		</h1>
+	)
 }
 export default Heading
