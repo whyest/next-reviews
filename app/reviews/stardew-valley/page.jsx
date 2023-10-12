5be965d116dc66d9b661549d32095b4617c7c8d6
whyest <whyest@gmail.com>
2023-10-12 17:26:38 +0300

    refactor review page code

--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -1,17 +1,5 @@
-import { readFile } from "node:fs/promises";
-import { marked } from "marked";
-import matter from "gray-matter";
 import Heading from "@/components/Heading";
-
-const getReview = async (slug) => {
-  const text = await readFile(`./content/reviews/${slug}.md`, "utf8");
-  const {
-    content,
-    data: { title, date, image },
-  } = matter(text);
-  const body = marked(content, { headerIds: false, mangle: false });
-  return { title, date, image, body };
-};
+import { getReview } from "@/lib/reviews";
 
 const StardewValleyPage = async () => {
   const review = await getReview("stardew-valley");
