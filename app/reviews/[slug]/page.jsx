76861510ba9dad6a843dab6f68a1004709090bcf
whyest <whyest@gmail.com>
2023-10-30 16:55:39 +0300

    on demand revalidation

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -31,15 +31,16 @@ const ReviewPage = async ({ params: { slug } }) => {
     <>
       <Heading>{review.title}</Heading>
       <p className="pb-3 font-semibold">{review.subtitle}</p>
-      <Link
-        href={"/reviews"}
-        className="my-2 inline-block rounded border px-2 py-1 text-sm capitalize text-slate-500 duration-200 hover:bg-orange-100 hover:text-slate-700"
-      >
-        Back to reviews
-      </Link>
-      <div className="mb-3 flex items-baseline gap-3">
-        <p className="pb-2 italic">{review.date}</p>
+
+      <div className="mb-3 flex items-center gap-3">
+        <p className="italic">{review.date}</p>
         <ShareButtons />
+        <Link
+          href={"/reviews"}
+          className="flex rounded border px-2 py-1 text-sm capitalize text-slate-500 duration-200 hover:bg-orange-100 hover:text-slate-700"
+        >
+          Back to reviews
+        </Link>
       </div>
 
       <Image
