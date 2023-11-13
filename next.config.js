949b717ad3302efbf310078ec15396815ccd5e25
whyest <whyest@gmail.com>
2023-11-13 17:05:28 +0300

    add env variables and configurable image source

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig } */
 module.exports = {
-  // output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-        pathname: "/uploads/**",
-      },
-    ],
+    remotePatterns: [toRemotePattern(process.env.CMS_IMAGE_PATTERN)],
   },
 };
+
+function toRemotePattern(urlString) {
+  const url = new URL(urlString);
+  return {
+    protocol: url.protocol.replace(":", ""),
+    hostname: url.hostname,
+    port: url.port,
+    pathname: url.pathname,
+  };
+}
